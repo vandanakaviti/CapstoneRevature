7c34f157098cf0e6c5c751fcc53c4196a6fdac91
hp <hp@Vandana-windows>
2022-05-26 10:47:35 +0530

    vcc

---
+++ src/main/java/com/RailwayReservation/RailwayReservationApplication.java
@@ -0,0 +1,13 @@
+package com.RailwayReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RailwayReservation/RailwayReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RailwayReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
